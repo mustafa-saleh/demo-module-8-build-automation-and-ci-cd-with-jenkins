da19785369d61fb14f9f383cc8b65b8ff721818a
mustafa abdelgader <mustafa-saleh>
2026-07-31 16:59:13 +0400

    test github trigger

--- script.groovy
+++ script.groovy
@@ -19,7 +19,7 @@ def buildImage() {
 
 def deployApp() {
     echo "deploying the application..."
-    echo "deploying the application... test3"
+    echo "deploying the application... test4"
 }
 
 return this
